3abd20f468d492d8c1e0d341e173c07af6488162
why-thor <286763969@qq.com>
2019-10-06 01:40:54 +0800

    confilct

--- src/main/java/com/sxt/git/Happy.java
+++ src/main/java/com/sxt/git/Happy.java
@@ -2,10 +2,7 @@ package com.sxt.git;
 
 public class Happy {
  public static void main(String[] args) {
-<<<<<<< HEAD
 	System.out.println("right---- ");
-=======
 	System.out.println("left-----");
->>>>>>> branch 'master' of https://github.com/why-thor/git01.git
 }
 }
